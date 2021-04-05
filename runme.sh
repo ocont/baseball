7d8285843b40ead9a80680b9e8813c8089afa0a6
toconnell <thomas_oconnell@yahoo.com>
2021-04-05 10:07:11 -0400

    adding modules

--- runme.sh
+++ runme.sh
@@ -3,3 +3,6 @@ timestamp=`date +"%Y-%m-%d"`
 ./bbmain2021.py > reports/output.csv
 ./sort.py
 cp -rf reports/${timestamp}.csv reports/latest.csv
+git add reports/*
+git commit -am "adding modules"
+git push origin
